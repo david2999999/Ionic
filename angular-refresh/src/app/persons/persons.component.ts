a3b4f3d38cd8c99a824856ee1ec9df32482ed2e4
David Jiang <djiang86@binghamton.edu>
2021-03-01 14:54:31 -0500

    sending HTTP request

--- angular-refresh/src/app/persons/persons.component.ts
+++ angular-refresh/src/app/persons/persons.component.ts
@@ -1,17 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {PersonsService} from "./persons.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css']
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
+  private personListSubscribe: Subscription;
   personList: string[];
 
   constructor(private personsService: PersonsService) { }
 
   ngOnInit(): void {
-    this.personList = this.personsService.persons;
+    this.personListSubscribe = this.personsService.personsChanged.subscribe(persons => {
+      this.personList = persons;
+    });
+
+    this.personsService.fetchPersons();
+  }
+
+  onRemovePerson(name: string) {
+    this.personsService.removePerson(name);
+  }
+
+  ngOnDestroy(): void {
+    this.personListSubscribe.unsubscribe();
   }
 }
